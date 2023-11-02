94ea15500ae939d3c86b8fc3c904eb2cfc22c139
abidnk <abidnk34@gmail.com>
2023-11-02 21:10:40 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+<MDBRow className='row-cols-1 row-cols-md-3 g-1' style={{ display: 'flex', flexWrap: 'wrap', alignItems: 'stretch', gap: '0.5rem' }}>
+  <MDBCol>
+    <MDBCard className='h-100' style={{ width: '300px' }}>
+      <MDBCardImage
+        src='src/assets/img/Categories/Screenshot from 2023-11-02 09-45-14.png'
+        alt='...'
+        position='top'
+      />
+    </MDBCard>
+  </MDBCol>
+  <MDBCol>
+    <MDBCard className='h-100' style={{ width: '300px' }}>
+      <MDBCardImage
+        src='src/assets/img/Categories/Screenshot from 2023-11-02 09-45-27.png'
+        alt='...'
+        position='top'
+      />
+    </MDBCard>
+  </MDBCol>
+  <MDBCol>
+    <MDBCard className='h-100' style={{ width: '300px' }}>
+      <MDBCardImage
+        src='src/assets/img/Categories/Screenshot from 2023-11-02 09-47-43.png'
+        alt='...'
+        position='top'
+      />
+    </MDBCard>
+  </MDBCol>
+</MDBRow>
